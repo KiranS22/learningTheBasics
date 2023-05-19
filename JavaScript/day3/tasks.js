3b1e99c335a585a70b827603daddd6bf4fd2b997
Kiran <krnsundal@gmail.com>
2023-05-19 14:29:08 +0100

    Add nums task complete

--- JavaScript/day3/tasks.js
+++ JavaScript/day3/tasks.js
@@ -9,6 +9,7 @@ when button is clicked, it calculates factoral
 display bellow
 
 
+
 Create JavaScript Todo - add and delete functionality
 
 
@@ -16,3 +17,24 @@ Create JavaScript Todo - add and delete functionality
 Read about create Elemnt and append child
 
 */
+// functions for both tasks
+
+const factorals = (num) => {
+  let factors = 1;
+  for (let i = 1; i <= num; i++) {
+    factors *= i;
+  }
+  return factors;
+};
+
+const sumOfNums = (num1, num2) => {
+  return num1 + num2;
+};
+// ----------------------------------
+
+document.getElementById("add").addEventListener("click", () => {
+  let num1Val = Number(document.getElementById("num1").value);
+  let num2Val = Number(document.getElementById("num2").value);
+  let result = sumOfNums(num1Val, num2Val);
+  document.querySelector(".display").innerHTML = `<h2>${result}</h2>`;
+});
