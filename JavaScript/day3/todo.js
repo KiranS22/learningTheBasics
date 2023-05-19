66e9a1c1006c471a96d3296c49c5bccb4a006c55
Kiran <krnsundal@gmail.com>
2023-05-19 18:34:15 +0100

    vanilla Todo add and clear input working

--- JavaScript/day3/todo.js
+++ JavaScript/day3/todo.js
@@ -1,8 +1,21 @@
-
 /*
 Create JavaScript Todo - add and delete functionality
 
-
-
-Read about create Elemnt and append child
 */
+
+let Todos = [];
+document.addEventListener("change", () => {
+  let task = document.getElementById("taskImp").value;
+  Todos.push(task);
+});
+
+document.getElementById("add").addEventListener("click", (e) => {
+  e.preventDefault();
+  for (let i = 0; i < Todos.length; i++) {
+    const todos = Todos[i];
+    document.getElementsByClassName("display")[0].innerHTML += `<div> <p>${[
+      todos,
+    ]}</p>  <button>Delete</button></div>`;
+  }
+  document.getElementById("taskImp").value = "";
+});
