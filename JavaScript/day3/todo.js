63330ba198baee861545c5df0879191f88931416
Kiran <krnsundal@gmail.com>
2023-05-20 12:22:31 +0100

    add bug fixed

--- JavaScript/day3/todo.js
+++ JavaScript/day3/todo.js
@@ -4,18 +4,18 @@ Create JavaScript Todo - add and delete functionality
 */
 
 let Todos = [];
-document.addEventListener("change", () => {
-  let task = document.getElementById("taskImp").value;
-  Todos.push(task);
-});
 
 document.getElementById("add").addEventListener("click", (e) => {
   e.preventDefault();
-  for (let i = 0; i < Todos.length; i++) {
-    const todos = Todos[i];
-    document.getElementsByClassName("display")[0].innerHTML += `<div> <p>${[
-      todos,
-    ]}</p>  <button>Delete</button></div>`;
-  }
+  let task = document.getElementById("taskImp").value;
+  Todos.push(task);
   document.getElementById("taskImp").value = "";
+
+  let todoDiv = `<div> <p>${[
+    task,
+  ]}</p>  <button class="delete">Delete</button></div>`;
+
+  document.getElementsByClassName("display")[0].innerHTML += todoDiv;
 });
+
+console.log("Delete Buttons ", document.getElementsByClassName("delete"));
