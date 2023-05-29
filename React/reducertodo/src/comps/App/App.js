fa425747461552e8f9a1fd0525b71dc7ddae0c8f
Kiran <krnsundal@gmail.com>
2023-05-29 13:15:33 +0100

    Add task component with useReducer complete

---
+++ React/reducertodo/src/comps/App/App.js
@@ -0,0 +1,42 @@
+import React, { useReducer, useState } from "react";
+import "./App.css";
+import ShowTodos from "../ShowTodos/ShowTodos";
+
+function App() {
+  let initialState = { todos: [] };
+  const [inpVal, setInpVal] = useState("");
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "ADD":
+        return { todos: [...state.todos, action.payload] };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div className="App">
+      <h1>Add Task Using useReducer</h1>
+      <input
+        type="text"
+        value={inpVal}
+        onChange={(e) => setInpVal(e.target.value)}
+      />
+      <button onClick={() => dispatch({ type: "ADD", payload: inpVal })}>
+        {" "}
+        Add Task
+      </button>
+
+      <div>
+        <div>
+          {state.todos.map((todo) => (
+            <ShowTodos key={todo.id} todo={todo} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/reducertodo/src/comps/ShowTodos/ShowTodos.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ShowTodos = ({ todo }) => {
+  return (
+    <div>
+      <h3>{todo}</h3>
+    </div>
+  );
+};
+
+export default ShowTodos;
