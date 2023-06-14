de8f3cdb8a9736f862f3d581f6b7b628214708fa
Kiran <krnsundal@gmail.com>
2023-06-14 16:35:20 +0100

    Products and  regester complete

--- Express-postgresql/Routes/auth.js
+++ Express-postgresql/Routes/auth.js
@@ -1,26 +1,34 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const pool = require("../db");
 // HashSync - syncronus
 // Hash - is async so requires await keyword
 const authRouter = express.Router();
 let users = [];
 authRouter.post("/register", async (req, res) => {
-  let { first_name, last_name, email, password } = req.body;
-  let salt = await bcrypt.genSalt(10); // runs the cycle of password encrytion
-  let hash = await bcrypt.hash(password, salt); //Coverts into hashed string
-  let emailExists = users.find((user) => user.email === email);
-  if (emailExists) {
-    res.send({ status: "err", message: "User already exists" });
-  } else {
-    let user = {
-      id: users.length + 1,
-      first_name,
-      last_name,
-      email,
-      password: hash,
-    };
-    users.push(user);
-    res.send("user added");
+  try {
+    let { first_name, last_name, email, password } = req.body;
+    let salt = await bcrypt.genSalt(10); // runs the cycle of password encrytion
+    let hash = await bcrypt.hash(password, salt); //Coverts into hashed string
+    const newUser = await pool.query(
+      "INSERT INTO users(first_name, last_name, email, password) VALUES($1,$2,$3,$4)",
+      [first_name, last_name, email, hash]
+    );
+    if (newUser) {
+      res.send({
+        data: newUser.rows[0],
+        status: "success",
+        message: "user registered",
+      });
+    } else {
+      res
+        .send({ status: "error", message: "Could not register user" })
+        .status(404);
+    }
+  } catch (e) {
+    res
+      .send({ status: "error", message: "Could not register user" })
+      .status(404);
   }
 });
 
